6e3504a93492790753cade45c14a409b6cdb2960
Kahu Leary <kahu.leary@outlook.com>
2021-03-11 01:48:02 +1300

    Add CartItem component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core'
 
-
+import CartItem from './CartItem/CartItem'
 import useStyles from './styles'
 
 const Cart = ({ cart }) => {
-    const isEmpty = !cart.line_items
     const classes = useStyles()
     
     const EmptyCart = () => (
@@ -17,8 +16,7 @@ const Cart = ({ cart }) => {
            <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        {/* <CartItem /> */}
-                        <div>{item.name}</div>
+                        <CartItem item={item} />
                     </Grid>
                 ))}
                 <div className={classes.cardDetails}>
@@ -41,12 +39,14 @@ const Cart = ({ cart }) => {
              </Grid>
           </> 
     )
+    
+    if(!cart.line_items) return 'Loading...'
 
     return (
         <Container>
             <div className={classes.toolbar} />
-            <Typography className={classes.title} variant='h3'>Your Shopping Cart</Typography>
-            { isEmpty ? <EmptyCart /> : <FilledCart /> }
+            <Typography className={classes.title} variant='h3' gutterBottom >Your Shopping Cart</Typography>
+            { !cart.line_items ? <EmptyCart /> : <FilledCart /> }
         </Container>
     )
 }
